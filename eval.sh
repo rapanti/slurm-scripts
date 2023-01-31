6147d47d17f3d7a8976ffdbde20fb50de6460685
rapanti <ivo.rapant@uranus.uni-freiburg.de>
2023-01-31 19:39:15 +0100

    quickpush

--- eval.sh
+++ eval.sh
@@ -3,9 +3,9 @@
 #SBATCH -t 23:59:59 # time (D-HH:MM:SS)
 #SBATCH --gres=gpu:4
 #SBATCH -D /work/dlclarge1/rapanti-stn_cifar/dino_cifar10
-#SBATCH -J eval_linear-test-cosine-epsilon-2-ep300
-#SBATCH -o /work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-test-cosine-epsilon-2-ep300/log/%A.%a.%N.out
-#SBATCH -e /work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-test-cosine-epsilon-2-ep300/log/%A.%a.%N.out
+#SBATCH -J eval_linear-stn-translation_scale_symmetric-thetacropspenalty-pretrain-exp3
+#SBATCH -o /work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-stn-translation_scale_symmetric-thetacropspenalty-pretrain-exp3/log/%A.%a.%N.out
+#SBATCH -e /work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-stn-translation_scale_symmetric-thetacropspenalty-pretrain-exp3/log/%A.%a.%N.out
 
 echo "Workingdir: $PWD";
 echo "Started at $(date)";
@@ -14,8 +14,8 @@ echo "Running job $SLURM_JOB_NAME with given JID $SLURM_JOB_ID on queue $SLURM_J
 source /home/rapanti/.profile
 source activate dino
 
-WEIGHTS=test-cosine-epsilon-2/checkpoint.pth
-EXP_D=/work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-test-cosine-epsilon-2-ep300
+WEIGHTS=dino-stn-translation_scale_symmetric-thetacropspenalty-pretrain-exp3/checkpoint.pth
+EXP_D=/work/dlclarge1/rapanti-stn_cifar/experiments/eval_linear-stn-translation_scale_symmetric-thetacropspenalty-pretrain-exp3
 # Job to perform
 torchrun \
   --nproc_per_node=4 \
@@ -26,10 +26,10 @@ torchrun \
     --dataset CIFAR10 \
     --data_path /work/dlclarge1/rapanti-stn_cifar/data/datasets/CIFAR10 \
     --pretrained_weights /work/dlclarge1/rapanti-stn_cifar/experiments/$WEIGHTS \
-    --img_size 32 \
-    --patch_size 4 \
+    --img_size 224 \
+    --patch_size 16 \
     --output_dir $EXP_D \
-    --epochs 300 \
+    --epochs 100 \
     --batch_size_per_gpu 192 \
     --lr 0.01 \
     --weight_decay 0 \
